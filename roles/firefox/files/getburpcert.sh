2d302ebe6baf15fcc903074ddfda4d75fbbdf552
Marc-Elvis <root@triplea.red>
2025-11-04 02:47:51 +0100

    Remove obsolete configuration files and update appearance settings for MATE desktop
    - Deleted xfwm4.xml and xsettings.xml as they are no longer needed.
    - Removed .wezterm.lua configuration file.
    - Added MATE desktop role to playbook and included appearance settings.
    - Deleted unused wallpaper setup task.
    - Updated appearance tasks to include VS Code configuration files.
    - Removed unnecessary keybindings and settings for MATE.
    - Cleaned up Go installation tasks and ensured proper version management.
    - Adjusted terminal settings and ensured proper permissions for dumpcap in Wireshark.
    - Added CopyQ desktop entry for autostart.
    - Added VS Code keybindings and settings JSON files.
    - Introduced MATE terminal settings via dconf.

--- roles/firefox/files/getburpcert.sh
+++ roles/firefox/files/getburpcert.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-burp=$(find / -name burps*.jar 2>/dev/null | tail -1)
-/bin/bash -c "timeout 45 /bin/java -Djava.awt.headless=true -jar $burp < <(echo y) &" 
+burp=$(find / -name burp*.jar 2>/dev/null | tail -1)
+/bin/bash -c "timeout 45 /usr/share/burpsuite/jre/bin/java -Djava.awt.headless=true -jar $burp < <(echo y) &" 
 sleep 30
 curl http://localhost:8080/cert -o /tmp/cacert.der
 exit
-
